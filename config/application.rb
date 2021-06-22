b97fbd4cc274aa7a1b0740163332dc53be62b868
sgigli <simon.luciano@gigli.net>
2021-06-22 13:49:40 -0400

    compact directory load

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,7 @@ module TennisNews
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.autoload_paths += Dir[Rails.root.join("app", "models", "{*/}")]
-    config.autoload_paths += Dir[Rails.root.join("app", "models", "{*/*/}")]
-    config.autoload_paths += Dir[Rails.root.join("app", "models", "{*/*/*/}")]
+    config.autoload_paths += Dir[Rails.root.join("app", "models", "{**/}")]
 
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
   end
