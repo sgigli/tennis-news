e2203665e56bc1385aa75881d384cedacd192759
sgigli <simon.luciano@gigli.net>
2021-06-22 12:33:12 -0400

    add associations to RemoveSeedData

--- app/services/remove_seed_data.rb
+++ app/services/remove_seed_data.rb
@@ -3,5 +3,6 @@ class RemoveSeedData
     Player.all.each { |player| player.destroy }
     Country.all.each { |country| country.destroy }
     Article.all.each { |article| article.destroy }
+    Association.all.each { |association| association.destroy }
   end
 end
