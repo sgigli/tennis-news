c107d407346765b1d5f83985e3b70ac23f756912
sgigli <simon.luciano@gigli.net>
2021-06-24 13:50:15 -0400

    update singles_matches seed

--- db/seeds/singles_matches.rb
+++ db/seeds/singles_matches.rb
@@ -5,7 +5,7 @@ french_open = Tournament.find_by(name: "French Open")
 
 matches = [
   {
-    score: "6-7(6-8),2-6,6-3,6-2,6-4",
+    score: "6-7,2-6,6-3,6-2,6-4",
     winner_id: djokovic.id,
     date: Date.new(2021,06,13),
     player_1_id: djokovic.id,
