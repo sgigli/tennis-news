41b009ea2c71d62c001df3c15f380fbac616a019
sgigli <simon.luciano@gigli.net>
2021-06-24 13:50:37 -0400

    init recent matches views

--- app/models/player_models/singles_match.rb
+++ app/models/player_models/singles_match.rb
@@ -2,4 +2,8 @@ class SinglesMatch < ApplicationRecord
   belongs_to :player_1, :class_name => :Player
   belongs_to :player_2, :class_name => :Player
   belongs_to :tournament
+
+  def sets
+    score.split(",").map { |set| set.split("-") }
+  end
 end
