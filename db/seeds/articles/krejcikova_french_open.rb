696c21a2015c66e0a8fb4b89df996b2332fac658
sgigli <simon.luciano@gigli.net>
2021-07-01 23:17:11 -0400

    add more articles and first article images

---
+++ db/seeds/articles/krejcikova_french_open.rb
@@ -0,0 +1,68 @@
+krejcikova_french_open = {
+  link: "https://www.theguardian.com/sport/2021/jun/13/barbora-krejcikova-doubles-up-at-french-open-to-make-mark-on-history",
+  site: "www.theguardian.com",
+  site_name: "The Guardian",
+  author: "Tumaini Carayol",
+  title: "Barbora Krejcikova doubles up at French Open to make mark on history",
+  subtitle: "Krejcikova and Katerina Siniakova win women’s doubles title. 
+            Czech is first player to win both Paris titles since Mary Pierce",
+  date: Date.new(2021, 6, 13),
+  content: "<p>Not since the 2000 edition of the French Open, when Mary Pierce followed 
+            her poignant, emotional singles revival by victoriously teaming up with the great 
+            Martina Hingis in doubles, had anyone done the double, winning singles and doubles 
+            titles at Roland Garros.</p>
+            <p>But over the past few days, Barbora Krejcikova has made the feat look easy. 
+            One day after a life-changing afternoon in her tennis career when she beat Anastasia 
+            Pavlyuchenkova to win a surprise first singles grand slam title, the Czech returned 
+            to the same court with her longtime partner, Katerina Siniakova, and they defeated 
+            Bethanie Mattek-Sands and Iga Swiatek 6-4, 6-2 in a match between the two most recent 
+            singles champions.</p>
+            <p>Krejcikova, 25, will now return to No 1 in the doubles rankings and after 
+            contesting 15 matches over the past few weeks and spending nearly 22 hours on 
+            court, including a quarter-final finish in the mixed doubles, she may now have 
+            a rest.</p>
+            <p>Afterwards, Krejcikova affirmed she will have to celebrate her double with 
+            some champagne, and some time at home, but Wimbledon is already near: “I’m just 
+            really looking forward to playing the next tournament and every tournament,” she 
+            said. “Right now, after all of this happened these last two weeks, also last days, 
+            I just feel really relieved and relaxed.</p>
+            <p>“I just know that from now on I can really enjoy it because I have pretty 
+            much achieved everything I always wanted. Now I can just improve. That’s the 
+            only thing I can do. Just keep improving as all the guys and all the ladies 
+            are doing.”</p>
+            <p>The comparison between Pierce and Krejcikova underlines the rarity of 
+            Krejcikova’s achievement. Pierce herself was a prodigiously talented player 
+            who was competing alongside one of the all-time great doubles players in Hingis. 
+            Krejcikova is treading a completely opposite route. She was simply returning to 
+            the discipline that was her primary source of income, pride and success for so 
+            long.</p>
+            <p>Krejcikova and Siniakova are unique. Doubles players do not usually reach 
+            No 1 at 22 years old, but they were one of the most dominant junior doubles 
+            teams in history. They teamed up for the first time at the junior French 
+            Open in 2013 simply because they had no one else to play with and they won 
+            the whole thing. They went on to win junior Wimbledon and the US Open that 
+            year, recording a 24-0 win-loss record in juniors as a team. By 2018 they 
+            had reached No 1 on the WTA, winning the French Open and Wimbledon.</p>
+            <p>For so much of their time together, Siniakova has been the better singles 
+            player. While Krejcikova struggled to break into the top 100, Siniakova easily 
+            reached the top 50 in 2016 aged 20, peaking at 31st, and has not left the 
+            top 100 since.</p>
+            <p>And yet it is Krejcikova who became the sixth consecutive first-time female 
+            grand slam champion to be crowned at Roland Garros and the third triumphant 
+            unseeded player during this period. She is probably the biggest surprise of 
+            all of those surprises but it is also a reminder that chaos rarely comes out 
+            of nothing.</p>
+            <p>Eight months ago at Roland Garros, she made her debut and reached the 
+            fourth round. Since then, she has established herself in the top 100. She 
+            reached the final of Dubai, the WTA 1000 tournament, without dropping a 
+            set in March.</p>
+            <p>In Rome a few weeks ago Krejcikova had Swiatek on the ropes, holding two 
+            match points before eventually losing 3-6, 7-6(5), 7-5. Then she won her first 
+            title in Strasbourg just before Roland Garros began.</p>
+            <p>She arrived in Paris full of confidence in her game and it allowed her game 
+            to flow in the good moments, to hold up through the tough ones and to mark her as 
+            a grand slam champion in both singles and doubles.
+            </p>"
+}
+
+Article.create!(krejcikova_french_open)
---
+++ db/seeds/articles/tsitsipas_monte_carlo.rb
@@ -0,0 +1,60 @@
+tsitsipas_monte_carlo = {
+  link: "https://www.atptour.com/en/news/tsitsipas-rublev-monte-carlo-2021-final",
+  site: "www.atptour.com",
+  site_name: "ATP Tour",
+  author: "ATP Staff",
+  title: "Tsitsipas Wins Maiden Masters 1000 Crown In Monte-Carlo",
+  subtitle: "Greek now leads the 2021 FedEx ATP Race To Turin for spot at Nitto ATP Finals",
+  date: Date.new(2021, 6, 13),
+  content: "<p>Third time proved to be the charm for Stefanos Tsitsipas at the Rolex 
+            Monte-Carlo Masters on Sunday when he collected his first ATP Masters 1000 
+            title with a 6-3, 6-3 victory against Andrey Rublev.</p>
+            <p>Appearing in his third final at the level, Tsitsipas dropped just four points 
+            behind his first serve (24/28) to capture the second biggest title of his career 
+            after 71 minutes. As a result of his final win, the 2019 Nitto ATP Finals champion 
+            will climb to first position in the FedEx ATP Race To Turin on Monday. The six-time 
+            ATP Tour titlist did not drop a set throughout his week at the Monte-Carlo Country 
+            Club.</p>
+            <p>“I had an unbelievable week in Monte-Carlo,” Tsitsipas said in his post-match 
+            interview. “I can’t describe my feelings right now. I am overwhelmed by so many 
+            different emotions and nostalgia. It is incredible that I am able to be in the 
+            position that I am. We both deserved to be in the final. We put on an amazing 
+            fight and an amazing show… I would consider it as the [best] week of my life so 
+            far.”</p>
+            <p>Tsitsipas claimed early breaks in each set and dominated on serve to narrow 
+            the gap on Rublev at the top of the 2021 wins leaderboard. The Monte-Carlo finalists 
+            currently occupy the top two positions on the list and are the only players to have 
+            claimed 20 or more victories this season.</p>
+            <p>Tsitsipas has taken a 4-3 lead in his fiercely contested ATP Head2Head rivalry 
+            with Rublev. The 23-year-old gained revenge for his semi-final loss to the Russian 
+            at last month’s ABN AMRO World Tennis Tournament in Rotterdam. Tsitsipas has won his 
+            past two clay-court encounters against Rublev, following his straight-sets quarter-final 
+            win at Roland Garros last year.</p>
+            <p>“I knew that he was going to be a very difficult opponent to face. There were a lot 
+            of nerves coming into that match. [We] always want to bring the best out of [ourselves] 
+            when facing each other. Playing Andrey was a very difficult thing to do today, also 
+            considering that it was a final. There is extra stress and extra importance in the 
+            match that we had to play. [I am] definitely proud of the way I managed to behave and 
+            be out on the court today.</p>
+            <p>“The clay court season couldn’t have started better,” Tsitsipas said. “It is the 
+            best thing winning my first Masters 1000 and it is even more special doing it here 
+            on home soil in Monte-Carlo and doing it on clay, which is my favourite surface.”</p>
+            <p>Tsitsipas made a fast start on Court Rainier III. The Athens native pinned Rublev 
+            into his backhand corner to earn his first break point of the match at 1-0 and switched 
+            tactics with a backhand up the line to claim the only break of the set. Tsitsipas dropped 
+            just two points across his next four service games (16/18) to move one set from 
+            victory.</p>
+            <p>Tsitsipas showcased a mix of power and precision on his backhand and found 
+            consistent depth on his forehand return to break for the second time at 1-1 in 
+            the second set. The World No. 5 continued his fine serving performance to reach 
+            match point, when he broke Rublev for the third time. Tsitsipas sunk to his knees 
+            in celebration after Rublev fired a cross-court backhand into the tramline.</p>
+            <p>Rublev was also chasing his first Masters 1000 trophy and the top spot in the 
+            FedEx ATP Race To Turin. The World No. 8 defeated Top 10 seeds Roberto Bautista 
+            Agut and 11-time champion Rafael Nadal in back-to-back three-set matches en route 
+            to his first Masters 1000 final.</p>
+            <p>Tsitsipas is not the first member of his family to triumph at the Monte-Carlo 
+            Country Club. His mother, Julia Salnikova, won a junior title at the venue in 1981.</p>"
+}
+
+Article.create!(tsitsipas_monte_carlo)
