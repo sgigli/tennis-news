5b8e3c2b2cd147b2049e984910e2052bb8a4f5c6
sgigli <simon.luciano@gigli.net>
2021-07-12 22:32:06 -0400

    init complete tournament show

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,5 +1,9 @@
 class TournamentsController < ApplicationController
   def index
-    @tournaments = Tournament.all
+    @tournaments = Tournament.order_by_start_date
+  end
+
+  def show
+    @tournament = Tournament.find(params[:id])
   end
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -2,4 +2,17 @@ class Tournament < ApplicationRecord
   has_many :singles_matches, dependent: :destroy
 
   has_one_attached :image
+
+  ROUNDS = ["finals", "semi-finals", "quarter-finals", "fourth", "third", "second", "first"]
+
+  def self.order_by_start_date
+    all.order("start_date desc")
+  end
+
+  def singles_matches_by_round
+    ROUNDS.map do |round|
+      matches = singles_matches.where(round: round)
+      matches.present? ? [round, matches] : nil
+    end.reject(&:nil?).to_h
+  end
 end
