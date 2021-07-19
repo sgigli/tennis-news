5449d51c17c1168f0e7188ec375a3c5c641b9936
sgigli <simon.luciano@gigli.net>
2021-07-19 18:12:27 -0400

    add order_by_date and gender methods to match concern; update view

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,7 +1,7 @@
 class HomePageController < ApplicationController
   def index
     @articles = Article.order_by_date
-    @recent_matches = SinglesMatch.all
+    @recent_matches = SinglesMatch.order_by_date
     @atp_players = TennisAssociation.atp&.players
     @wta_players = TennisAssociation.wta&.players
   end
--- app/models/concerns/match_concern.rb
+++ app/models/concerns/match_concern.rb
@@ -10,6 +10,15 @@ module MatchConcern
     score.split(",").map { |set| set.split("-") }
   end
 
+  def gender
+    case tennis_association
+    when "ATP"
+      "Men's"
+    when "WTA"
+      "Women's"
+    end
+  end
+
   class_methods do
     ROUNDS = ["finals", "semi-finals", "quarter-finals", "fourth", "third", "second", "first"]
 
@@ -18,6 +27,10 @@ module MatchConcern
         matches = where(round: round)
         matches.present? ? [round, matches] : nil
       end.reject(&:nil?).to_h
+    end
+
+    def order_by_date
+      all.order("date desc")
     end
   end
 end
